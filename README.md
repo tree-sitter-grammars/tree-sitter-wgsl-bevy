c81dc770310795caea5e00996505deba024ec698
Stephan Seitz <stephan.seitz@fau.de>
2022-12-25 17:36:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+tree-sitter-wgsl-bevy
+=====================
+
+This is a extension of [tree-sitter-wgsl](https://github.com/szebniok/tree-sitter-wgsl) to support
+the [Bevy](https://bevyengine.org/) preprocessor.
+
+Work in progress...
---
+++ grammar.js
@@ -0,0 +1,107 @@
+const WGSL = require("tree-sitter-wgsl/grammar")
+
+module.exports = grammar(WGSL, {
+    name: 'wgsl_bevy',
+
+    rules: {
+        _declaration: ($, original) => choice(
+            $.preproc_import,
+            $.define_import_path,
+            $.preproc_ifdef,
+            //$.preproc_if,
+            original,
+        ),
+
+        _statement: ($, original) => choice(
+            $.preproc_import,
+            alias($.preproc_ifdef_in_statement, $.preproc_ifdef),
+            //$.preproc_if,
+            original,
+        ),
+
+        struct_declaration: $ => seq(
+            "struct",
+            field("name", $.identifier),
+            "{",
+            $._struct_declaration_content,
+            "}"
+        ),
+
+        _struct_declaration_content: $ => seq(
+            repeat(choice(seq($.struct_member, ","), $.preproc_import, alias($.preproc_ifdef_in_struct_declaration, $.preproc_ifdef))),
+            choice($.struct_member, $.preproc_import, alias($.preproc_ifdef_in_struct_declaration, $.preproc_ifdef)),
+            optional(",")),
+
+        preproc_import: $ => seq(
+            preprocessor('import'),
+            field('path', choice(
+                $.import_path,
+            )),
+            '\n'
+        ),
+        define_import_path: $ => seq(
+            preprocessor('define_import_path'),
+            field('path', choice(
+                $.import_path,
+            )),
+            '\n'
+        ),
+        import_path: $ => doubleColonSep1($.identifier),
+
+        ...preprocIf('', $ => $._declaration),
+        ...preprocIf('_in_statement', $ => $._statement),
+        ...preprocIf('_in_struct_declaration', $ => seq(choice($.struct_member, $.preproc_import, alias($.preproc_ifdef_in_struct_declaration, $.preproc_ifdef)),
+            optional(","))),
+    }
+});
+
+// from tree-sitter-c
+function preprocessor(command) {
+    return alias(new RegExp('#[ \t]*' + command), '#' + command)
+}
+
+// from tree-sitter-c
+function preprocIf(suffix, content) {
+    function elseBlock($) {
+        return choice(
+            suffix ? alias($['preproc_else' + suffix], $.preproc_else) : $.preproc_else,
+            //suffix ? alias($['preproc_elif' + suffix], $.preproc_elif) : $.preproc_elif,
+        );
+    }
+
+    return {
+        //['preproc_if' + suffix]: $ => seq(
+        //preprocessor('if'),
+        //field('condition', $._preproc_expression),
+        //'\n',
+        //repeat(content($)),
+        //field('alternative', optional(elseBlock($))),
+        //preprocessor('endif')
+        //),
+
+        ['preproc_ifdef' + suffix]: $ => seq(
+            choice(preprocessor('ifdef'), preprocessor('ifndef')),
+            field('name', $.identifier),
+            repeat(content($)),
+            field('alternative', optional(elseBlock($))),
+            preprocessor('endif')
+        ),
+
+        ['preproc_else' + suffix]: $ => seq(
+            preprocessor('else'),
+            repeat(content($))
+        ),
+
+        //['preproc_elif' + suffix]: $ => seq(
+        //preprocessor('elif'),
+        //field('condition', $._preproc_expression),
+        //'\n',
+        //repeat(content($)),
+        //field('alternative', optional(elseBlock($))),
+        //)
+    }
+}
+
+function doubleColonSep1(rule) {
+    return seq(rule, repeat(seq('::', rule)))
+}
