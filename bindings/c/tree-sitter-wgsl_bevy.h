e50b8ab8fb59c5a6f9b33770b4d44a18e7dbf4b0
Stephan Seitz <sseitz@nvidia.com>
2024-04-06 21:37:23 +0200

    feat: add bindings for C, Python, Go

---
+++ bindings/c/tree-sitter-wgsl_bevy.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_WGSL_BEVY_H_
+#define TREE_SITTER_WGSL_BEVY_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_wgsl_bevy(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_WGSL_BEVY_H_
---
+++ bindings/python/tree_sitter_wgsl_bevy/__init__.py
@@ -0,0 +1,5 @@
+"WgslBevy grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
